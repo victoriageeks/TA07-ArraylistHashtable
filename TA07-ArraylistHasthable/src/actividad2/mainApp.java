7a851039b809f77f6a8558c3a973fb892c76465e
Victoria Geeks ♡ <victoriageeks@gmail.com>
2022-10-11 01:46:14 +0200

    actividad2

--- TA07-ArraylistHasthable/src/actividad2/mainApp.java
+++ TA07-ArraylistHasthable/src/actividad2/mainApp.java
@@ -1,5 +1,8 @@
 package actividad2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class mainApp {
 
 	public static void main(String[] args) {
@@ -12,7 +15,85 @@ public class mainApp {
 		 *  - número de artículos comprados
 		 *  - cantidad pagada
 		 *  - cambio a devolver al cliente*/
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//Arraylist Productos
+		ArrayList<Double> productos = new ArrayList<>();
+		
+		
+		//Cantidad comprada de productos
+		System.out.println("Introduce la cantidad de galletas que quieras comprar:");
+		Integer galletas = sc.nextInt();
+		
+		System.out.println("Introduce la cantidad de patatas que quieras comprar:");
+		Integer patatas = sc.nextInt();
+		
+		System.out.println("Introduce la cantidad de palomitas que quieras comprar:");
+		Integer palomitas = sc.nextInt();
+		
+		System.out.println("Introduce la cantidad de pañuelos que quieras comprar:");
+		Integer panuelos = sc.nextInt();
+		
+		System.out.println("Introduce la cantidad de toallitas que quieras comprar:");
+		Integer toallitas = sc.nextInt();
+		
+		int numProductos = galletas + patatas + palomitas + panuelos + toallitas;
+		System.out.println("\nCantidad de productos comprada: " + numProductos+".\n");
+		
+		//calcular precio bruto
+		double galletas_PB = galletas*0.75;
+		double patatas_PB = patatas*1.89;
+		double palomitas_PB = palomitas*2.25;
+		double panuelos_PB = panuelos*1.50;
+		double toallitas_PB = toallitas*2.30;
+		System.out.println("--- PRECIO BRUTO PRODUCTOS ---");
+		System.out.println("Galletas : "+galletas_PB+" euros.\nPatatas: "+patatas_PB+" euros.\nPalomitas: "+palomitas_PB+" euros.\nPañuelos: "+panuelos_PB+" euros.\nToallitas: "+toallitas_PB+" euros.\n");
+		
+		//añadimos al arraylist los productos
+		productos.add(galletas_PB);
+		productos.add(patatas_PB);
+		productos.add(palomitas_PB);
+		productos.add(panuelos_PB);
+		productos.add(toallitas_PB);
+		
+		//calcular el precio con iva
+		double galletas_IVA = galletas_PB * 1.04;
+		double patatas_IVA = patatas_PB * 1.04;
+		double palomitas_IVA = palomitas_PB * 1.04;
+		double panuelos_IVA = panuelos_PB * 1.21;
+		double toallitas_IVA = toallitas_PB * 1.21;
+		System.out.println("--- PRECIO PRODUCTOS + IVA ---");
+		System.out.println("Galletas : "+galletas_IVA+" euros.\nPatatas: "+patatas_IVA+" euros.\nPalomitas: "+palomitas_IVA+" euros.\nPañuelos: "+panuelos_IVA+" euros.\nToallitas: "+toallitas_IVA+" euros.\n");
+		
+		
+		//Calcular precio total a pagar
+		double precioTotal = galletas_IVA + patatas_IVA + palomitas_IVA + panuelos_IVA + toallitas_IVA;
+		System.out.println("---PARTE FINAL DE LA COMPRA---");
+		System.out.println("Debe pagar: "+precioTotal+" euros.");
+		
+		double totalPagado = 0;
+		double pagar = 0;
+		double faltaPagar = 0;
+		double devolver = 0;
+		
+		//Momento de pagar
+		while(totalPagado<precioTotal) {
+			System.out.println("Indique la cantidad de dinero que va a pagar:");
+			pagar = sc.nextDouble();
+			totalPagado+=pagar;
+			
+			if(totalPagado>precioTotal) {
+				devolver = totalPagado - precioTotal;
+				System.out.println("El cambio a devolver es: " + devolver + " euros.");
+			} else {
+				faltaPagar = precioTotal - totalPagado;
+				System.out.println("Falta por pagar: " + faltaPagar + " euros.");
+			}
+		}
+		
+		
+		System.out.println("Arraylist de la lista de productos: "+productos.toString());
 	
 	}
 
--- TA07-ArraylistHasthable/src/actividad3/mainApp.java
+++ TA07-ArraylistHasthable/src/actividad3/mainApp.java
@@ -1,11 +1,15 @@
 package actividad3;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class mainApp {
 
 	public static void main(String[] args) {
 		/* Crea una base de datos de 10 artículos para controlar el stock de productos
 		 * de una tienda por medio de un diccionario de datos (articulo:precio).
-		 * El usuario podrá añadir, por edio de interfaz visual artículos
+		 * El usuario podrá añadir, por medio de interfaz visual, artículos
 		 * nuevos y cantidades de estos. El usuario podrá consultar información
 		 * almacenada en el diccionario referente a un artículo concreto
 		 * e incluso listar toda la información en la terminal del programa.*/
